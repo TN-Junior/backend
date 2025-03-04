b8dcbfd277328cf8bc9fb21a5c63174fca703be7
TaryJr <>
2025-03-04 16:37:00 -0300

    update

--- app.py
+++ app.py
@@ -103,5 +103,5 @@ def reset_participants():
     return jsonify({"message": "Participants reset successfully"}), 200
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
 
