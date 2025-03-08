78a247a887db9813cf86e72ec1b7de907d2a78cd
TaryJr <>
2025-03-07 22:47:52 -0300

    teste

--- app/config.py
+++ app/config.py
@@ -2,23 +2,21 @@ import os
 from dotenv import load_dotenv
 from urllib.parse import quote_plus
 
-# Carregar as variáveis do .env
+# Carregar as variáveis do arquivo .env
 load_dotenv()
 
-# Debugging - Verificar se a variável de ambiente está carregada corretamente
-print("DEBUG - DB_PASSWORD:", os.getenv('DB_PASSWORD'))
-
 class Config:
-    db_password = os.getenv('DB_PASSWORD')
-
-    if db_password is None:
-        raise ValueError("Erro: A variável de ambiente DB_PASSWORD não está definida.")
-
-    # Usar quote_plus para garantir que caracteres especiais sejam tratados corretamente
-    db_password = quote_plus(db_password)
+    # Obtem a senha diretamente sem usar encode, pois quote_plus pode lidar com strings
+    db_password = quote_plus(os.getenv('DB_PASSWORD'))  
 
     SQLALCHEMY_DATABASE_URI = (
         f"mysql://{os.getenv('DB_USER')}:{db_password}"
         f"@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
     )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test_db.sqlite'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
